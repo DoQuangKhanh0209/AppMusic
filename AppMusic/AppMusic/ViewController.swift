904c61196e638a56473af050e8395a80a6650d16
Đỗ Quang Khánh <do.quang.khanh@sun-asterisk.com>
2022-10-10 09:12:55 +0700

    initialize and draw the music player interface

---
+++ AppMusic/AppMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMusic
+//
+//  Created by Quang Khánh on 10/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
